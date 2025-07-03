fea168c8533818145a817c626e768f8824fd4c0b
Yeray Cabello <j.cabello.ramos@gmail.com>
2025-07-03 10:42:15 +0200

    .

--- src/Nvx.ConsistentAPI.Tests/StandardFlowTest.cs
+++ src/Nvx.ConsistentAPI.Tests/StandardFlowTest.cs
@@ -308,16 +308,16 @@ public class StandardFlowTest
       await setup.Command(new RegisterFavoriteFood("pizza"), true);
       await setup.Command(new RegisterFavoriteFood("banana"));
       await setup.WaitForConsistency();
-        var adminFavoriteFoods = await setup.ReadModels<UserFavoriteFoodReadModel>(true);
-        Assert.Equal(1, adminFavoriteFoods.Total);
-        Assert.Contains(adminFavoriteFoods.Items, model => model.Name == "pizza");
-        var nonAdminFavoriteFoods = await setup.ReadModels<UserFavoriteFoodReadModel>();
-        Assert.Equal(1, nonAdminFavoriteFoods.Total);
-        Assert.Contains(nonAdminFavoriteFoods.Items, model => model.Name == "banana");
-        await setup.ReadModel<UserFavoriteFoodReadModel>(setup.Auth.AdminSub, asAdmin: true);
-        await setup.ReadModelNotFound<UserFavoriteFoodReadModel>(setup.Auth.AdminSub);
-        await setup.ReadModel<UserFavoriteFoodReadModel>(setup.Auth.CandoSub, asAdmin: false);
-        await setup.ReadModelNotFound<UserFavoriteFoodReadModel>(setup.Auth.CandoSub, asAdmin: true);
+      var adminFavoriteFoods = await setup.ReadModels<UserFavoriteFoodReadModel>(true);
+      Assert.Equal(1, adminFavoriteFoods.Total);
+      Assert.Contains(adminFavoriteFoods.Items, model => model.Name == "pizza");
+      var nonAdminFavoriteFoods = await setup.ReadModels<UserFavoriteFoodReadModel>();
+      Assert.Equal(1, nonAdminFavoriteFoods.Total);
+      Assert.Contains(nonAdminFavoriteFoods.Items, model => model.Name == "banana");
+      await setup.ReadModel<UserFavoriteFoodReadModel>(setup.Auth.AdminSub, asAdmin: true);
+      await setup.ReadModelNotFound<UserFavoriteFoodReadModel>(setup.Auth.AdminSub);
+      await setup.ReadModel<UserFavoriteFoodReadModel>(setup.Auth.CandoSub, asAdmin: false);
+      await setup.ReadModelNotFound<UserFavoriteFoodReadModel>(setup.Auth.CandoSub, asAdmin: true);
     }
 
     async Task Idempotency()
