5562153227543afc503c9e34dcc4f08653504f02
Yeray Cabello <j.cabello.ramos@gmail.com>
2025-07-24 11:30:47 +0100

    .

--- src/Nvx.ConsistentAPI.EventStore.EventStoreDB/EventStoreDbStore.cs
+++ src/Nvx.ConsistentAPI.EventStore.EventStoreDB/EventStoreDbStore.cs
@@ -1,3 +1,30 @@
-﻿namespace Nvx.ConsistentAPI.EventStore.EventStoreDB;
+﻿using DeFuncto;
+using Nvx.ConsistentAPI.EventStore.Store;
 
-public class EventStoreDbStore { }
+namespace Nvx.ConsistentAPI.EventStore.EventStoreDB;
+
+public class EventStoreDbStore : EventStore<EventModelEvent>
+{
+  public Task Initialize(CancellationToken cancellationToken = default) => throw new NotImplementedException();
+
+  public AsyncResult<InsertionSuccess, InsertionFailure> Insert(InsertionPayload<EventModelEvent> payload) =>
+    throw new NotImplementedException();
+
+  public IAsyncEnumerable<ReadAllMessage> Read(
+    ReadAllRequest request = default,
+    CancellationToken cancellationToken = default) => throw new NotImplementedException();
+
+  public IAsyncEnumerable<ReadStreamMessage<EventModelEvent>> Read(
+    ReadStreamRequest request,
+    CancellationToken cancellationToken = default) => throw new NotImplementedException();
+
+  public IAsyncEnumerable<ReadAllMessage> Subscribe(
+    SubscribeAllRequest request = default,
+    CancellationToken cancellationToken = default) => throw new NotImplementedException();
+
+  public IAsyncEnumerable<ReadStreamMessage<EventModelEvent>> Subscribe(
+    SubscribeStreamRequest request,
+    CancellationToken cancellationToken = default) => throw new NotImplementedException();
+
+  public Task TruncateStream(StrongId id, ulong truncateBefore) => throw new NotImplementedException();
+}
