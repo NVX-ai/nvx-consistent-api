a7074b39e46f66517bee34e1bce3c4012f88bf64
Yeray Cabello <j.cabello.ramos@gmail.com>
2025-08-28 10:53:22 +0200

    Fix hydration on unparseable events

---
+++ src/Nvx.ConsistentAPI/ConsistentApp.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Nvx.ConsistentAPI;
+
+public sealed class ConsistentApp(WebApplication webApp, Fetcher fetcher) : IAsyncDisposable
+{
+  public WebApplication WebApp { get; } = webApp;
+  public Fetcher Fetcher { get; } = fetcher;
+
+  public async ValueTask DisposeAsync() => await WebApp.DisposeAsync();
+
+  public void Run() => WebApp.Run();
+  public async Task StartAsync() => await WebApp.StartAsync();
+}
